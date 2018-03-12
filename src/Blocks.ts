0d6f06e65d764933b9ba1188ed6965ecc1e25ef9
vincentOpenSource <13916486874@163.com>
2018-03-12 15:08:25 +0800

    格式化

--- src/Blocks.ts
+++ src/Blocks.ts
@@ -1,40 +1,30 @@
 // import * as $ from "jquery";
 /// <reference types="jquery" />
 
-namespace WebBrowser
-{
-    export class BlockPage
-    {
-        constructor()
-        {
-            $("#searchBtn").click(() =>
-            {
+namespace WebBrowser {
+    export class BlockPage {
+        constructor() {
+            $("#searchBtn").click(() => {
                 window.location.href = './blockInfo.html?index=' + $("#searchText").val();
             });
         }
-        public async updateBlocks(pageUtil: PageUtil)
-        {
+        public async updateBlocks(pageUtil: PageUtil) {
             let ajax: Ajax = new Ajax();
             let blocks: Block[] = await ajax.post('getblocks', [pageUtil.pageSize, pageUtil.currentPage]);
             console.log("blocks-page");
             $("#blocks-page").children("table").children("tbody").empty();
-            if (pageUtil.totalPage - pageUtil.currentPage)
-            {
+            if (pageUtil.totalPage - pageUtil.currentPage) {
                 $("#blocks-page").find("#next").removeClass('disabled');
-            } else
-            {
+            } else {
                 $("#blocks-page").find("#next").addClass('disabled');
             }
-            if (pageUtil.currentPage - 1)
-            {
+            if (pageUtil.currentPage - 1) {
                 $("#blocks-page").find("#previous").removeClass('disabled');
-            } else
-            {
+            } else {
                 $("#blocks-page").find("#previous").addClass('disabled');
             }
             let newDate = new Date();
-            blocks.forEach((item, index, input) =>
-            {
+            blocks.forEach((item, index, input) => {
                 newDate.setTime(item.time * 1000);
                 let html: string;
                 html += '<tr><td>'
@@ -45,8 +35,7 @@ namespace WebBrowser
             });
         }
 
-        public async queryBlock(index: number)
-        {
+        public async queryBlock(index: number) {
             let ajax: Ajax = new Ajax();
             let newDate = new Date();
             let blocks: Block[] = await ajax.post('getblock', [index]);
@@ -59,8 +48,7 @@ namespace WebBrowser
             $("#version").text(block.version);
             $("#index").text(block.index);
             let txs: Tx[] = block.tx;
-            txs.forEach(tx =>
-            {
+            txs.forEach(tx => {
                 $("#txs").append('<tr><td><a href="./txInfo.html?txid=' + tx.txid + '">' + tx.txid + '</a></td><td>' + tx.type + '</td><td>' + tx.size + ' bytes</td><td>' + tx.version + '</td></tr>');
             });
 
